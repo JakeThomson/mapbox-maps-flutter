336e2ea9b34731eaa6feb1de2080fe879c7524c3
Jake Thomson <jakethomson0@gmail.com>
2026-01-23 02:38:04 -0800

    revert resize changes

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/ViewAnnotationController.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/ViewAnnotationController.kt
@@ -151,28 +151,8 @@ class ViewAnnotationController(
         container.visibility = View.INVISIBLE
         rootView.addView(container)
 
-        // Create remeasure callback for this annotation
-        val remeasureCallback: () -> Unit = {
-            if (annotations.containsKey(id)) {
-                container.post {
-                    val newWidth = container.width
-                    val newHeight = container.height
-                    if (newWidth > 0 && newHeight > 0) {
-                        val updateOptions = viewAnnotationOptions {
-                            width(newWidth)
-                            height(newHeight)
-                        }
-                        viewAnnotationManager.updateViewAnnotation(container, updateOptions)
-                    }
-                }
-            }
-        }
-
         composeView.setContent {
-            CompositionLocalProvider(
-                LocalViewAnnotationVisible provides visibilityState,
-                LocalRequestRemeasure provides remeasureCallback
-            ) {
+            CompositionLocalProvider(LocalViewAnnotationVisible provides visibilityState) {
                 factory(data ?: emptyMap())
             }
         }
@@ -285,28 +265,8 @@ class ViewAnnotationController(
         container.visibility = View.INVISIBLE
         rootView.addView(container)
 
-        // Create remeasure callback for this annotation
-        val remeasureCallback: () -> Unit = {
-            if (annotations.containsKey(id)) {
-                container.post {
-                    val newWidth = container.width
-                    val newHeight = container.height
-                    if (newWidth > 0 && newHeight > 0) {
-                        val updateOptions = viewAnnotationOptions {
-                            width(newWidth)
-                            height(newHeight)
-                        }
-                        viewAnnotationManager.updateViewAnnotation(container, updateOptions)
-                    }
-                }
-            }
-        }
-
         composeView.setContent {
-            CompositionLocalProvider(
-                LocalViewAnnotationVisible provides visibilityState,
-                LocalRequestRemeasure provides remeasureCallback
-            ) {
+            CompositionLocalProvider(LocalViewAnnotationVisible provides visibilityState) {
                 factory(data ?: emptyMap())
             }
         }
