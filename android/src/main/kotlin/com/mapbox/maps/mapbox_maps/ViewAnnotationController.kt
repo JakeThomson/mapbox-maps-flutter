d97de7a6aae3b075c58c65e31765a2390ed3d45b
Jake Thomson <jakethomson0@gmail.com>
2026-01-22 20:47:13 -0800

    Revert "Measure widget and set view size"
    This reverts commit 7ea2f7877161b718c4340b5d573e0561d5946861.

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/ViewAnnotationController.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/ViewAnnotationController.kt
@@ -147,8 +147,6 @@ class ViewAnnotationController(
         // Wait for composition and layout
         composeView.post {
             composeView.post {
-                updateMeasuredSize(container)
-                
                 // Remove from root view
                 rootView.removeView(container)
                 container.visibility = View.VISIBLE
@@ -246,8 +244,6 @@ class ViewAnnotationController(
         // Wait for composition and layout
         composeView.post {
             composeView.post {
-                updateMeasuredSize(container)
-                
                 // Remove from root view
                 rootView.removeView(container)
                 container.visibility = View.VISIBLE
@@ -351,18 +347,6 @@ class ViewAnnotationController(
             "BOTTOM_RIGHT" -> ViewAnnotationAnchor.BOTTOM_RIGHT
             "CENTER" -> ViewAnnotationAnchor.CENTER
             else -> ViewAnnotationAnchor.CENTER
-        }
-    }
-    
-    private fun updateMeasuredSize(container: FrameLayout) {
-        val widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-        val heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-        container.measure(widthSpec, heightSpec)
-        val measuredWidth = container.measuredWidth
-        val measuredHeight = container.measuredHeight
-        if (measuredWidth > 0 && measuredHeight > 0) {
-            container.layout(0, 0, measuredWidth, measuredHeight)
-            container.layoutParams = ViewGroup.LayoutParams(measuredWidth, measuredHeight)
         }
     }
     
